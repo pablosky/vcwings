370804d2c297c1980e5ab4d6e86548e5fa3eba5c
pablo perez <pablo.sky@hotmail.com>
2013-10-04 10:49:46 -0300

    clearance fix1

--- config/routes.rb
+++ config/routes.rb
@@ -23,25 +23,25 @@ Prueba2::Application.routes.draw do
 
   #resource :session, controller: 'sessions'
 
-  resources :passwords,
-    :controller => 'clearance/passwords',
-    :only => [:create, :new]
+  # resources :passwords,
+  #   :controller => 'clearance/passwords',
+  #   :only => [:create, :new]
 
-  resource :session,
-    :controller => 'clearance/sessions',
-    :only => [:create]
+  # resource :session,
+  #   :controller => 'clearance/sessions',
+  #   :only => [:create]
 
-  resources :users,
-    :controller => 'clearance/users',
-    :only => [:create] do
-      resource :password,
-        :controller => 'clearance/passwords',
-        :only => [:create, :edit, :update]
-    end
+  # resources :users,
+  #   :controller => 'clearance/users',
+  #   :only => [:create] do
+  #     resource :password,
+  #       :controller => 'clearance/passwords',
+  #       :only => [:create, :edit, :update]
+  #   end
 
-  get '/sign_in' => 'clearance/sessions#new', :as => 'sign_in'
-  delete '/sign_out' => 'clearance/sessions#destroy', :as => 'sign_out'
-  get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
+  # get '/sign_in' => 'clearance/sessions#new', :as => 'sign_in'
+  # delete '/sign_out' => 'clearance/sessions#destroy', :as => 'sign_out'
+  # get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
 
 
 
